1caf0bd59b9e9b0938ce2e452c14c89665b731aa
arsathmohamed <arsathmohamed351@gmail.com>
2026-08-13 18:13:44 +0530

    DBT Step Completed

---
+++ DBT/walmart_dbt_project/analyses/stores.sql
@@ -0,0 +1,1 @@
+SELECT * FROM {{ ref('stores_raw') }}
---
+++ DBT/walmart_dbt_project/models/curated/ephemeral/customer.sql
@@ -0,0 +1,16 @@
+SELECT 
+    DISTINCT
+    customer_id,
+    customer_first_name,
+    customer_last_name,
+    customer_full_name,
+    customer_segment,
+    customer_city,
+    customer_province,
+    customer_created_timestamp,
+    customer_updated_timestamp,
+    customer_is_active,
+    customer_country,
+    current_timestamp() AS curated_customer_processed_at
+FROM 
+    {{ ref('OBT') }}
